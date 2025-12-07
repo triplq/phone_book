2c63ee75641747f2179468ec34355391866b8664
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2025-12-08 02:11:00 +0300

    third

--- hello.go
+++ hello.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Person struct {
 	Name string
@@ -16,6 +19,29 @@ func AddContact(person Person, number string) *Contact {
 	return &Contact{
 		Person: person,
 		Number: number,
+	}
+}
+
+func FindContact(contacts []Contact, name string) (int, *Contact) {
+	for i, val := range contacts {
+		if val.Name == name {
+			return i, &val
+		}
+	}
+	return -1, nil
+}
+
+func DeleteContact(contacts []Contact, name string) []Contact {
+	ind, _ := FindContact(contacts, name)
+	new_cont := make([]Contact, 0, len(contacts)-1)
+	new_cont = append(new_cont, contacts[:ind]...)
+	new_cont = append(new_cont, contacts[ind+1:]...)
+	return new_cont
+}
+
+func PrintAll(contacts []Contact) {
+	for _, val := range contacts {
+		fmt.Println("Name: " + val.Name + " Age: " + strconv.Itoa(val.Age) + " Number: " + val.Number)
 	}
 }
 
@@ -37,6 +63,14 @@ func (p *Person) BD() {
 func main() {
 	var contacts []Contact
 	contacts = append(contacts, *AddContact(*NewPerson("Tom", 24), "89040928902"))
-	contacts[0].BD()
-	fmt.Println(contacts)
+	contacts = append(contacts, *AddContact(*NewPerson("Alice", 12), "4123434"))
+	contacts = append(contacts, *AddContact(*NewPerson("Sasha", 98), "3434343"))
+	contacts = append(contacts, *AddContact(*NewPerson("Alex", 4), "4111040"))
+
+	ind, p_Alex := FindContact(contacts, "Alex")
+	fmt.Println(ind, p_Alex)
+
+	contacts = DeleteContact(contacts, "Alex")
+
+	PrintAll(contacts)
 }
